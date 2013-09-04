9eb9b10521ba0ba5659fdd83c67524da3fdd40e6
Evan Swick <eswick@live.com>
2013-09-04 17:31:51 -0400

    Fixed OSAppWindow orientation issues.

--- OSAppWindow.m
+++ OSAppWindow.m
@@ -22,12 +22,39 @@
 	self.appView.frame = frame;
 	[self addSubview:self.appView];
 
+
 	return self;
 }
 
 - (void)layoutSubviews{
 	[super layoutSubviews];
-	self.appView.transform = CGAffineTransformScale(CGAffineTransformIdentity, self.bounds.size.width / self.appView.bounds.size.width, (self.bounds.size.height - self.windowBar.bounds.size.height) / self.appView.bounds.size.height);
+
+	int appViewDegrees;
+
+	switch([self.application statusBarOrientation]){
+		case UIInterfaceOrientationPortrait:
+			appViewDegrees = 0;
+			break;
+		case UIInterfaceOrientationPortraitUpsideDown:
+			appViewDegrees = 180;
+			break;
+		case UIInterfaceOrientationLandscapeLeft:
+			appViewDegrees = 90;
+			break;
+		case UIInterfaceOrientationLandscapeRight:
+			appViewDegrees = 270;
+			break;
+	}
+
+
+	self.appView.transform = CGAffineTransformRotate(CGAffineTransformIdentity, DegreesToRadians(appViewDegrees));
+
+	if UIDeviceOrientationIsLandscape([self.application statusBarOrientation]){
+		self.appView.transform = CGAffineTransformScale(self.appView.transform, (self.bounds.size.height - self.windowBar.bounds.size.height) / self.appView.bounds.size.width, self.bounds.size.width / self.appView.bounds.size.height);
+	}else{
+		self.appView.transform = CGAffineTransformScale(self.appView.transform, self.bounds.size.width / self.appView.bounds.size.width, (self.bounds.size.height - self.windowBar.bounds.size.height) / self.appView.bounds.size.height);
+	}
+
 	CGRect frame = self.appView.frame;
 	frame.origin = CGPointZero;
 	frame.origin.y += self.windowBar.bounds.size.height;
